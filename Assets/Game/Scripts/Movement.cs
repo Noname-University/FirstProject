279801e8b9214673456a8a4934282d93de23a9f1
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-03-02 20:58:14 +0300

    Gold and Player added

---
+++ Assets/Game/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+     private float speed;
+
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        var gold = other.GetComponent<Gold>();
+        if (gold != null)
+        {
+            gold.Collect();
+        }
+    }
+}
