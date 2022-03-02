65f7ef9d2544d0ce110310165aa3b2860ad10b87
Onurcan Koçak <onurcn_kck75@hotmail.com>
2022-03-02 20:59:12 +0300

    Ilk Deneyim

---
+++ Assets/Game/Scripts/Gold.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    [SerializeField]
+
+    private float speed;
+  private void Update() {
+      transform.Rotate(0,speed * Time.deltaTime ,0);
+  }
+    public void Collect()
+    {
+        Destroy(gameObject);
+    }
+}
