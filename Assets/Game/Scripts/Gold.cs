0dd63885c651c3662309b27857caba7382f7e6ac
Enes Yeşilyurt <yslyrt.enes@gmail.com>
2022-03-02 20:59:08 +0300

    Prefabs added

---
+++ Assets/Game/Scripts/Gold.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+   
+    private void Update()
+    {
+        transform.Rotate(0,speed * Time.deltaTime,0);
+    }
+    public void Collect()
+    {
+        Destroy(gameObject);
+    }
+}
