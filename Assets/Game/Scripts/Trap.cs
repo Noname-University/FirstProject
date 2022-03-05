8c9b7077d7c9d5cac325eee12b841f74c855e8c7
Enes Yeşilyurt <yslyrt.enes@gmail.com>
2022-03-05 20:42:52 +0300

    collectables added

--- Assets/Game/Scripts/Trap.cs
+++ Assets/Game/Scripts/Trap.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gold : MonoBehaviour, ICollectable
+public class Trap : MonoBehaviour, ICollectable
 {
     [SerializeField]
+    private float damage;
 
-    private float speed;
-  private void Update() {
-      transform.Rotate(0,speed * Time.deltaTime ,0);
-  }
     public void Collect()
     {
+        Player.Instance.Healthdecraese(damage);
         Destroy(gameObject);
     }
 }
