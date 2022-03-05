1e4289b773a8e696d84ed6444c2ed93811551f72
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-03-05 20:41:36 +0300

    Player refactor

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Helpers;
 using UnityEngine;
-
-public class Player : MonoBehaviour
+public class Player : MonoSingleton<Player>, IKillable
 {
+
    [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private float jumpSpeed;
+
+    [SerializeField]
+    private Transform jumpControlPoint;
+
+    [SerializeField]
+
+    private float health;
+
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        transform.position += new Vector3(
-            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-            0,
-            Input.GetAxis("Vertical") * speed * Time.deltaTime
-        );
+        Movement();
     }
 
     private void   OnTriggerEnter(Collider other) 
@@ -21,6 +35,38 @@ public class Player : MonoBehaviour
         if(collectable != null)
         {
             collectable.Collect();
+        }
+    }
+
+    //sağ sol zıplama ileri geri
+    private void Movement(){
+        //sağ sol ileri geri hareket
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            0,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime
+        );
+        //zıpmalama
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.Raycast(jumpControlPoint.position, Vector3.down, 0.1f))
+            {
+                rb.AddForce(Vector3.up * jumpSpeed);
+            }
+        }
+    }
+
+    public void Kill()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void HealtDecraese(float hitPoint)
+    {
+        health -= hitPoint;
+        if (health <= 0)
+        {
+            Kill();
         }
     }
 }
